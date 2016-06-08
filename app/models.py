4d22612cabed576a9b4afb22a05101ab9cfec5a1
Ryan Schuetzler <ryan@schuetzler.net>
2016-06-08 15:27:52 -0500

    I can log in and out now!

--- app/models.py
+++ app/models.py
@@ -2,9 +2,28 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    nickname = db.Column(db.String(64), index = True, unique = True)
+    # social_id = db.Column(db.string(64), nullable = False, unique = True)
+    nickname = db.Column(db.String(64), index = True)
     email = db.Column(db.String(120), index = True, unique = True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)
+        except NameError:
+            return str(self.id)
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
