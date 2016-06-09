ad44f11fa14de7e438f7980a9477ed9e3b3e4c68
Ryan Schuetzler <ryan@schuetzler.net>
2016-06-09 11:39:33 -0500

    Put following onto the pages

--- app/views.py
+++ app/views.py
@@ -53,6 +53,9 @@ def after_login(resp):
         user = User(nickname = nickname, email = resp.email)
         db.session.add(user)
         db.session.commit()
+        # Users should follow themselves
+        db.session.add(user.follow(user))
+        db.session.commit()
     remember_me = False
     if 'remember_me'in session:
         remember_me = session['remember_me']
@@ -109,6 +112,52 @@ def edit():
         form.about_me.data = g.user.about_me
     return render_template('edit.djhtml', form=form)
 
+@app.route('/follow/<nickname>')
+@login_required
+def follow(nickname):
+    user = User.query.filter_by(nickname = nickname).first()
+    if user is None:
+        flash('User %s not found' % nickname)
+        return redirect(url_for('index'))
+
+    if user == g.user:
+        flash('You can\'t follow yourself!')
+        return redirect(url_for('user', nickname = nickname))
+
+    u = g.user.follow(user)
+    if u is None:
+        flash('Cannot follow %s.' % nickname)
+        return redirect(url_for('user', nickname = nickname))
+
+    db.session.add(u)
+    db.session.commit()
+    flash('You are now following %s!' % nickname)
+    return redirect(url_for('user', nickname = nickname))
+
+@app.route('/unfollow/<nickname>')
+@login_required
+def unfollow(nickname):
+    user = User.query.filter_by(nickname = nickname).first()
+    if user is None:
+        flash('User %s not found' % nickname)
+        return redirect(url_for('index'))
+
+    if user == g.user:
+        flash('You can\'t unfollow yourself!')
+        return redirect(url_for('user', nickname = nickname))
+
+    u = g.user.unfollow(user)
+    if u is None:
+        flash('Cannot unfollow %s.' % nickname)
+        return redirect(url_for('user', nickname = nickname))
+
+    db.session.add(u)
+    db.session.commit()
+    flash('You have stopped following %s.' % nickname)
+    return redirect(url_for('user', nickname = nickname))
+
+
+# Error handling
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.djhtml'), 404
